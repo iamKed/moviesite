ba188be8c2ecf6b9db6e1f10b102cc7d1aeb2c78
iamKed <veerkrushnadalvi@orchidengg.ac.in>
2024-02-13 15:16:57 +0530

    kedar redux topper

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React by Kedar Swami the great 
         </a>
       </header>
     </div>
